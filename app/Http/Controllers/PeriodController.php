f4d2b60727d2b41dccf53e510fced96250e8e74f
Michael Dyer <mrmichaeldyer@gmail.com>
2018-12-24 12:09:27 +0000

    Update heading to reflect set range

--- app/Http/Controllers/PeriodController.php
+++ app/Http/Controllers/PeriodController.php
@@ -10,7 +10,7 @@ class PeriodController extends Controller
     public function __construct(Request $request)
     {
         $this->middleware(function ($request, $next) {
-            $this->period = session('period') ?: getPeriod();
+            $this->period = session('period') ? : getPeriod();
 
             return $next($request);
         });
@@ -33,42 +33,50 @@ class PeriodController extends Controller
     public function preset(Request $preset)
     {
         $end = Carbon::now();
+        $period_label = 'this month';
         switch ($preset->preset) {
             case '7days':
                 $start = Carbon::now()->subWeek();
-
+                $period_label = 'the last 7 days';
                 break;
             case '1mth':
                 $start = Carbon::now()->subMonth();
+                $period_label = 'the last 30 days';
 
                 break;
             case '3mths':
                 $start = Carbon::now()->subMonths(3);
+                $period_label = 'the last 90 days';
 
                 break;
             case '1year':
                 $start = Carbon::now()->subYear();
+                $period_label = 'the last year';
 
                 break;
             case 'week':
                 $start = Carbon::now()->startOfWeek();
+                $period_label = 'this week';
 
                 break;
             case 'month':
                 $start = Carbon::now()->startOfMonth();
+                $period_label = 'this month';
 
                 break;
             case 'year':
                 $start = Carbon::now()->startOfYear();
+                $period_label = 'this year';
 
                 break;
             default:
                 $start = Carbon::now()->startOfMonth();
+                $period_label = 'this month';
 
                 break;
         }
         $preset->session()->put('period', [$start, $end]);
-
+        $preset->session()->put('period_label', $period_label);
         $preset->flash('message', 'Date range updated');
 
         return redirect('/');
