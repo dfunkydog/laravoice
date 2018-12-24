3cd105bfe1e114902d58d31b3afbed0a781b4603
Michael Dyer <mrmichaeldyer@gmail.com>
2018-12-24 11:00:30 +0000

    Change button classes in peroid select

--- resources/js/components/SetPeriod.vue
+++ resources/js/components/SetPeriod.vue
@@ -5,27 +5,27 @@
       <form action="period" method="POST">
         <input type="hidden" name="_token" :value="token">
         <input type="hidden" name="preset" value="7days">
-        <button class="naked" type="submit">Last 7 Days</button>
+        <button class="btn btn--incognito" type="submit">Last 7 Days</button>
       </form>
       <form action="period" method="POST">
         <input type="hidden" name="_token" :value="token">
         <input type="hidden" name="preset" value="1mth">
-        <button class="naked" type="submit">Last 30 Days</button>
+        <button class="btn btn--incognito" type="submit">Last 30 Days</button>
       </form>
       <form action="period" method="POST">
         <input type="hidden" name="_token" :value="token">
         <input type="hidden" name="preset" value="3mths">
-        <button class="naked" type="submit">Last 90 Days</button>
+        <button class="btn btn--incognito" type="submit">Last 90 Days</button>
       </form>
       <form action="period" method="POST">
         <input type="hidden" name="_token" :value="token">
         <input type="hidden" name="preset" value="month">
-        <button class="naked" type="submit">This month</button>
+        <button class="btn btn--incognito" type="submit">This month</button>
       </form>
       <form action="period" method="POST">
         <input type="hidden" name="_token" :value="token">
         <input type="hidden" name="preset" value="week">
-        <button class="naked" type="submit">This week</button>
+        <button class="btn btn--incognito" type="submit">This week</button>
       </form>
     </div>
   </modal>
