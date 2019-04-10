345fd8dc6555c45657d303e972c0c47d920f0761
Michael Dyer <mrmichaeldyer@gmail.com>
2019-04-11 00:16:19 +0100

    Vendor Add date and sort descending

--- resources/views/vendor/show.blade.php
+++ resources/views/vendor/show.blade.php
@@ -2,13 +2,13 @@
 @section('content')
 <section class="section">
     <h1>Total spent at {{$vendor->name}} {{ session('period_label') ?: 'this month'}} {!! money($expenses->sum('amount')) !!}
-        </h1>
+    </h1>
     <ul class="catlist">
         @foreach ($expenses as $expense)
         <li>
             <a class="catlist__item" href="{{action('ExpenseController@show', ['id' => $expense->id])}}" style="background-size: {{ $expense->amount *100 /$expenses->max('amount')  }}% {{config( 'view.depth') }} "><strong>{{strtoupper($expense->type->name)}}</strong>
                 <div class="catlist__count">
-                    {{$expense->description}}
+                    {{$expense->description}} <span class="text-transluscent">{{$expense->paid_on}}</span>
                 </div>
                 <div class="catlist__amount pill">
                     {!! money($expense->amount) !!}
